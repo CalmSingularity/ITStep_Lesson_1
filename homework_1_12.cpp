49560b7be13a8a35196910b3fdd5d20ebf062d8c
Semen Klimov <simon-klimov@yandex.ru>
2016-06-26 01:40:21 +0300

    Task 12 fixed

--- homework_1_12.cpp
+++ homework_1_12.cpp
@@ -7,11 +7,11 @@ int gcd(int a, int b) {
   if (a == 0) return b;
   if (b == 0) return a;
   if (a == b) return a;
-  if (a == 1 or b == 1) return 1;
-  if (a % 2 == 0 and b % 2 == 0) return 2 * gcd(a/2, b/2);
-  if (a % 2 == 0 and b % 2 == 1) return gcd(a/2, b);
-  if (a % 2 == 1 and b % 2 == 0) return gcd(a, b/2);
-  if (a % 2 == 1 and b % 2 == 1) {
+  if (a == 1 || b == 1) return 1;
+  if (a % 2 == 0 && b % 2 == 0) return 2 * gcd(a/2, b/2);
+  if (a % 2 == 0 && b % 2 == 1) return gcd(a/2, b);
+  if (a % 2 == 1 && b % 2 == 0) return gcd(a, b/2);
+  if (a % 2 == 1 && b % 2 == 1) {
     if (a > b) return gcd(b, (a-b)/2);
     else return gcd(a, (b-a)/2);
   }
